3302e283cff9c33ed89e868c905a103bbe0e92b2
Marcos Dias <59514474+MDias13@users.noreply.github.com>
2023-06-03 06:48:47 -0300

    Find by id para tecnico

--- src/main/java/com/marcos/helpdesk/config/DevConfig.java
+++ src/main/java/com/marcos/helpdesk/config/DevConfig.java
@@ -2,6 +2,7 @@ package com.marcos.helpdesk.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,15 +10,25 @@ import org.springframework.context.annotation.Profile;
 import com.marcos.helpdesk.services.DBservice;
 
 @Configuration
-@Profile("test")
-public class TestConfig{
+@Profile("dev")
+public class DevConfig{
 
 	@Autowired
 	private DBservice dbservice;
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
 	
 	@Bean
-	public void instanciaDB() {
-		this.dbservice.instanciaDB();
+	public boolean instanciaDB() {
+		
+		if(value.equals("create")) {
+			this.dbservice.instanciaDB();
+			
+		}
+		return false;
+		
+		
+		
 	}
 	
 }
